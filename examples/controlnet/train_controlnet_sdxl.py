a4447a5c41fd013698aede87192cd09dfdadca20
moti <moti@scintium.com>
2024-02-14 18:38:41 +0200

    finished testing locally

--- examples/controlnet/train_controlnet_sdxl.py
+++ examples/controlnet/train_controlnet_sdxl.py
@@ -128,8 +128,6 @@ def log_validation(vae, unet, controlnet, args, accelerator, weight_dtype, step)
             {"validation_image": validation_image, "images": images, "validation_prompt": validation_prompt}
         )
 
-    pipeline.to("cpu")
-
     for tracker in accelerator.trackers:
         if tracker.name == "tensorboard":
             for log in image_logs:
@@ -166,9 +164,9 @@ def log_validation(vae, unet, controlnet, args, accelerator, weight_dtype, step)
         else:
             logger.warn(f"image logging not implemented for {tracker.name}")
 
-    # del pipeline
-    # gc.collect()
-    # torch.cuda.empty_cache()
+    del pipeline
+    gc.collect()
+    torch.cuda.empty_cache()
 
     return image_logs
 
